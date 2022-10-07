5c40a07243a9853b0723b69b6b170504f2d0f4dd
21bq1a4249 <113082224+21bq1a4249@users.noreply.github.com>
2022-10-07 09:23:10 +0530

    Add files via upload

---
+++ NestTry.java
@@ -0,0 +1,21 @@
+class NestTry{
+    public static void main(String args[]){
+       try{
+         int a=args.length;
+         int b=42/a;
+         System.out.print;ln("a= " + a);
+         try{
+           if(a==1 a=a/(a-a);
+           if(a==2){
+             int c[]={1};
+             c[42]=99;
+           }
+        }
+         catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("aIO: " + e);
+         }
+         catch(ArithematicException e){
+            System.out.println("divide by zero: "+ e);
+         }
+       }
+}
